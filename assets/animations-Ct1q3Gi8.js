5f2721af748ea29549488d30abf2d4e943bfb359
Camilo Beltran <camilobeltran@macbookpro.lan>
2025-07-12 17:57:00 -0500

    Updates

---
+++ assets/animations-Ct1q3Gi8.js
@@ -0,0 +1,1 @@
+const a={initial:{},animate:{transition:{staggerChildren:.2,delayChildren:.1}}},i={initial:{opacity:0,y:50},animate:{opacity:1,y:0,transition:{duration:.6,ease:"easeOut"}}};export{i as f,a as s};
